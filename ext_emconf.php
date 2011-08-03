7861c12d21283fee818fae70e0a5fa6e83318166
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-08-03 19:45:14 +0000

    Add jQuery 1.6.2 / Add jQuery UI 1.8.14 / Fix jsInFooter / Convert linefeed to Unix
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jftabulatorsitemap/trunk@50495 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "jftabulatorsitemap".
 #
-# Auto generated 25-06-2011 00:05
+# Auto generated 03-08-2011 21:44
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -29,19 +29,19 @@ $EM_CONF[$_EXTKEY] = array(
 	'clearCacheOnLoad' => 1,
 	'lockType' => '',
 	'author_company' => '',
-	'version' => '1.2.2',
+	'version' => '1.2.3',
 	'constraints' => array(
 		'depends' => array(
 			'cms' => '',
-			'php' => '5.0.0-5.3.99',
-			'typo3' => '4.3.0-4.5.99',
+			'php' => '5.0.0-0.0.0',
+			'typo3' => '4.3.0-4.99.999',
 		),
 		'conflicts' => array(
 		),
 		'suggests' => array(
 		),
 	),
-	'_md5_values_when_last_written' => 'a:44:{s:21:"ext_conf_template.txt";s:4:"8b02";s:12:"ext_icon.gif";s:4:"437c";s:17:"ext_localconf.php";s:4:"5816";s:14:"ext_tables.php";s:4:"0a53";s:13:"locallang.xml";s:4:"fd0c";s:16:"locallang_db.xml";s:4:"8c79";s:12:"t3jquery.txt";s:4:"93e0";s:24:"compat/flashmessages.css";s:4:"4e2c";s:20:"compat/gfx/error.png";s:4:"e4dd";s:26:"compat/gfx/information.png";s:4:"3750";s:21:"compat/gfx/notice.png";s:4:"a882";s:17:"compat/gfx/ok.png";s:4:"8bfe";s:22:"compat/gfx/warning.png";s:4:"c847";s:14:"doc/manual.sxw";s:4:"2ab3";s:43:"lib/class.tx_jftabulatorsitemap_tceFunc.php";s:4:"553c";s:47:"lib/class.tx_jftabulatorsitemap_tsparserext.php";s:4:"f7e1";s:14:"pi1/ce_wiz.gif";s:4:"b7d0";s:39:"pi1/class.tx_jftabulatorsitemap_pi1.php";s:4:"75c9";s:47:"pi1/class.tx_jftabulatorsitemap_pi1_wizicon.php";s:4:"e77e";s:19:"pi1/flexform_ds.xml";s:4:"015d";s:17:"pi1/locallang.xml";s:4:"5b7c";s:32:"res/tx_jftabulatorsitemap_pi1.js";s:4:"a516";s:55:"res/jquery/css/theme-1.8.13/jquery-ui-1.8.13.custom.css";s:4:"39d7";s:76:"res/jquery/css/theme-1.8.13/images/ui-bg_diagonals-thick_18_b81900_40x40.png";s:4:"95f9";s:76:"res/jquery/css/theme-1.8.13/images/ui-bg_diagonals-thick_20_666666_40x40.png";s:4:"f040";s:66:"res/jquery/css/theme-1.8.13/images/ui-bg_flat_10_000000_40x100.png";s:4:"c18c";s:67:"res/jquery/css/theme-1.8.13/images/ui-bg_glass_100_f6f6f6_1x400.png";s:4:"5f18";s:67:"res/jquery/css/theme-1.8.13/images/ui-bg_glass_100_fdf5ce_1x400.png";s:4:"d26e";s:66:"res/jquery/css/theme-1.8.13/images/ui-bg_glass_65_ffffff_1x400.png";s:4:"e5a8";s:73:"res/jquery/css/theme-1.8.13/images/ui-bg_gloss-wave_35_9f2614_500x100.png";s:4:"da5d";s:76:"res/jquery/css/theme-1.8.13/images/ui-bg_highlight-soft_100_eeeeee_1x100.png";s:4:"384c";s:75:"res/jquery/css/theme-1.8.13/images/ui-bg_highlight-soft_75_ffe45c_1x100.png";s:4:"b806";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_222222_256x240.png";s:4:"ebe6";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_228ef1_256x240.png";s:4:"79f4";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_65160b_256x240.png";s:4:"8250";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_ef8c08_256x240.png";s:4:"ef9a";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_ffd27a_256x240.png";s:4:"ab8c";s:62:"res/jquery/css/theme-1.8.13/images/ui-icons_ffffff_256x240.png";s:4:"342b";s:33:"res/jquery/js/jquery-1.6.1.min.js";s:4:"a34f";s:44:"res/jquery/js/jquery-ui-1.8.13.custom.min.js";s:4:"e232";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"a6f7";s:20:"static/constants.txt";s:4:"d8b4";s:16:"static/setup.txt";s:4:"a8ea";s:21:"static/ajax/setup.txt";s:4:"d55a";}',
+	'_md5_values_when_last_written' => 'a:44:{s:21:"ext_conf_template.txt";s:4:"d574";s:12:"ext_icon.gif";s:4:"437c";s:17:"ext_localconf.php";s:4:"1dab";s:14:"ext_tables.php";s:4:"b554";s:13:"locallang.xml";s:4:"fd0c";s:16:"locallang_db.xml";s:4:"2066";s:12:"t3jquery.txt";s:4:"6943";s:24:"compat/flashmessages.css";s:4:"4e2c";s:20:"compat/gfx/error.png";s:4:"e4dd";s:26:"compat/gfx/information.png";s:4:"3750";s:21:"compat/gfx/notice.png";s:4:"a882";s:17:"compat/gfx/ok.png";s:4:"8bfe";s:22:"compat/gfx/warning.png";s:4:"c847";s:14:"doc/manual.sxw";s:4:"ba24";s:43:"lib/class.tx_jftabulatorsitemap_tceFunc.php";s:4:"6e01";s:47:"lib/class.tx_jftabulatorsitemap_tsparserext.php";s:4:"f7e1";s:14:"pi1/ce_wiz.gif";s:4:"b7d0";s:39:"pi1/class.tx_jftabulatorsitemap_pi1.php";s:4:"83cf";s:47:"pi1/class.tx_jftabulatorsitemap_pi1_wizicon.php";s:4:"e77e";s:19:"pi1/flexform_ds.xml";s:4:"f907";s:17:"pi1/locallang.xml";s:4:"5b7c";s:32:"res/tx_jftabulatorsitemap_pi1.js";s:4:"a516";s:55:"res/jquery/css/theme-1.8.14/jquery-ui-1.8.14.custom.css";s:4:"f17d";s:76:"res/jquery/css/theme-1.8.14/images/ui-bg_diagonals-thick_18_b81900_40x40.png";s:4:"95f9";s:76:"res/jquery/css/theme-1.8.14/images/ui-bg_diagonals-thick_20_666666_40x40.png";s:4:"f040";s:66:"res/jquery/css/theme-1.8.14/images/ui-bg_flat_10_000000_40x100.png";s:4:"c18c";s:67:"res/jquery/css/theme-1.8.14/images/ui-bg_glass_100_f6f6f6_1x400.png";s:4:"5f18";s:67:"res/jquery/css/theme-1.8.14/images/ui-bg_glass_100_fdf5ce_1x400.png";s:4:"d26e";s:66:"res/jquery/css/theme-1.8.14/images/ui-bg_glass_65_ffffff_1x400.png";s:4:"e5a8";s:73:"res/jquery/css/theme-1.8.14/images/ui-bg_gloss-wave_35_9f2614_500x100.png";s:4:"da5d";s:76:"res/jquery/css/theme-1.8.14/images/ui-bg_highlight-soft_100_eeeeee_1x100.png";s:4:"384c";s:75:"res/jquery/css/theme-1.8.14/images/ui-bg_highlight-soft_75_ffe45c_1x100.png";s:4:"b806";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_222222_256x240.png";s:4:"ebe6";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_228ef1_256x240.png";s:4:"79f4";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_65160b_256x240.png";s:4:"27ac";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_ef8c08_256x240.png";s:4:"ef9a";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_ffd27a_256x240.png";s:4:"ab8c";s:62:"res/jquery/css/theme-1.8.14/images/ui-icons_ffffff_256x240.png";s:4:"342b";s:33:"res/jquery/js/jquery-1.6.2.min.js";s:4:"a1a8";s:44:"res/jquery/js/jquery-ui-1.8.14.custom.min.js";s:4:"4c2d";s:34:"res/jquery/js/jquery.easing-1.3.js";s:4:"6516";s:20:"static/constants.txt";s:4:"0b2b";s:16:"static/setup.txt";s:4:"5844";s:21:"static/ajax/setup.txt";s:4:"e8c7";}',
 	'suggests' => array(
 	),
 );

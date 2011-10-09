a92af5bc60025a215e7f09a42cc957a792408a22
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-10-09 14:52:11 +0000

    Add jQuery 1.6.4 / Add jQuery UI 1.8.16 / Fix performance issue
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jftabulatorsitemap/trunk@52834 735d13b6-9817-0410-8766-e36946ffe9aa

--- pi1/class.tx_jftabulatorsitemap_pi1.php
+++ pi1/class.tx_jftabulatorsitemap_pi1.php
@@ -52,6 +52,7 @@ class tx_jftabulatorsitemap_pi1 extends tslib_pibase
 	private $js = array();
 	private $cssFiles = array();
 	private $css = array();
+	private $piFlexForm = array();
 
 	/**
 	 * The main method of the PlugIn
@@ -373,7 +374,7 @@ class tx_jftabulatorsitemap_pi1 extends tslib_pibase
 				// Add script only once
 				$hash = md5($temp_js);
 				if ($this->conf['jsInline']) {
-					$GLOBALS['TSFE']->inlineJS[$hash] = $temp_css;
+					$GLOBALS['TSFE']->inlineJS[$hash] = $temp_js;
 				} elseif (t3lib_div::int_from_ver(TYPO3_version) >= 4003000) {
 					if ($this->conf['jsInFooter'] || $allJsInFooter) {
 						$pagerender->addJsFooterInlineCode($hash, $temp_js, $this->conf['jsMinify']);
@@ -507,6 +508,19 @@ class tx_jftabulatorsitemap_pi1 extends tslib_pibase
 	}
 
 	/**
+	* Set the piFlexform data
+	*
+	* @return void
+	*/
+	protected function setFlexFormData()
+	{
+		if (! count($this->piFlexForm)) {
+			$this->pi_initPIflexForm();
+			$this->piFlexForm = $this->cObj->data['pi_flexform'];
+		}
+	}
+
+	/**
 	 * Extract the requested information from flexform
 	 * @param string $sheet
 	 * @param string $name
@@ -515,30 +529,29 @@ class tx_jftabulatorsitemap_pi1 extends tslib_pibase
 	 */
 	protected function getFlexformData($sheet='', $name='', $devlog=true)
 	{
-		$this->pi_initPIflexForm();
-		$piFlexForm = $this->cObj->data['pi_flexform'];
-		if (! isset($piFlexForm['data'])) {
+		$this->setFlexFormData();
+		if (! isset($this->piFlexForm['data'])) {
 			if ($devlog === true) {
 				t3lib_div::devLog("Flexform Data not set", $this->extKey, 1);
 			}
 			return null;
 		}
-		if (! isset($piFlexForm['data'][$sheet])) {
+		if (! isset($this->piFlexForm['data'][$sheet])) {
 			if ($devlog === true) {
 				t3lib_div::devLog("Flexform sheet '{$sheet}' not defined", $this->extKey, 1);
 			}
 			return null;
 		}
-		if (! isset($piFlexForm['data'][$sheet]['lDEF'][$name])) {
+		if (! isset($this->piFlexForm['data'][$sheet]['lDEF'][$name])) {
 			if ($devlog === true) {
 				t3lib_div::devLog("Flexform Data [{$sheet}][{$name}] does not exist", $this->extKey, 1);
 			}
 			return null;
 		}
-		if (isset($piFlexForm['data'][$sheet]['lDEF'][$name]['vDEF'])) {
-			return $this->pi_getFFvalue($piFlexForm, $name, $sheet);
+		if (isset($this->piFlexForm['data'][$sheet]['lDEF'][$name]['vDEF'])) {
+			return $this->pi_getFFvalue($this->piFlexForm, $name, $sheet);
 		} else {
-			return $piFlexForm['data'][$sheet]['lDEF'][$name];
+			return $this->piFlexForm['data'][$sheet]['lDEF'][$name];
 		}
 	}
 

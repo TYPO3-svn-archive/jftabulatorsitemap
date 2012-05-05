88f1c2f1decb7ca123e76bb50186cc9c4a8816a1
juergen.furrer <juergen.furrer@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-05-05 16:30:17 +0000

    Remove deprecated methods (#36599) / Add jQuery 1.7.2 / Add jQuery UI 1.8.20
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/jftabulatorsitemap/trunk@61518 735d13b6-9817-0410-8766-e36946ffe9aa

--- lib/class.tx_jftabulatorsitemap_tsparserext.php
+++ lib/class.tx_jftabulatorsitemap_tsparserext.php
@@ -43,7 +43,7 @@ class tx_jftabulatorsitemap_tsparserext
 	{
 		$out = '';
 
-		if (t3lib_div::int_from_ver(TYPO3_version) < 4003000) {
+		if (t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version) < 4003000) {
 			// 4.3.0 comes with flashmessages styles. For older versions we include the needed styles here
 			$cssPath = $GLOBALS['BACK_PATH'] . t3lib_extMgm::extRelPath('jftabulatorsitemap');
 			$out .= '<link rel="stylesheet" type="text/css" href="' . $cssPath . 'compat/flashmessages.css" media="screen" />';
